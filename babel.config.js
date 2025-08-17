c56dbbd6fd7f5cf283ae9324d3596f4c3facd8f0
Hanif1dev <hanifyousufzai@tamkin.vc>
2025-08-17 09:00:44 +0000

    Remove bad babel.config.js

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-cat > babel.config.js <<'JS'
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
-  };
-};
-JS
